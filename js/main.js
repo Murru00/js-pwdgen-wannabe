6120b205b5313aa81d4285b9cf0f8bde59c6144d
Murru00 <marcomurruolbia@gmail.com>
2024-01-08 16:45:05 +0100

    add surname and color

--- js/main.js
+++ js/main.js
@@ -4,5 +4,20 @@ const resultElement = document.getElementById(`main-title`)
 
 //NAME
 
-const Name = parseInt (prompt ( 'Inserisci il tuo Nome'))
+const Name =  (prompt ( 'Inserisci il tuo Nome'))
 console.log(Name)
+
+
+
+//SURNAME
+
+const Surname =  (prompt ( 'Inserisci il tuo Cognome'))
+console.log(Surname)
+
+
+
+//COLOR
+
+const Color =  (prompt ( 'Inserisci il tuo colore preferito'))
+console.log(Color)
+
